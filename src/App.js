d2b85d5b5d365c4d37194abb04ad8e78db9432f3
Jacob Davis <jacobdavis608@gmail.com>
2020-03-30 16:11:47 -0400

    Starting web application off with skeleton code

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <p>Welcome to Yawn Mower!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
