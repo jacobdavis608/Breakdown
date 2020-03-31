85f6b480822be39aeecf94defc001b29df650313
Jacob Davis <jacobdavis608@gmail.com>
2020-03-31 15:17:09 -0400

    Changing launch screen

--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
 import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
+import logo from './bd_logo.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo}  />
+        
         <p>Welcome to Breakdown!</p>
       </header>
     </div>
