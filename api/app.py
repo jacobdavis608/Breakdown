27d8ff65be81d8ec366205416551bf69d3f6f6dd
Jacob Davis <jacobdavis608@gmail.com>
2020-04-12 14:16:54 -0400

    Adding some app routes for backend

--- api/app.py
+++ api/app.py
@@ -1,13 +1,15 @@
 import time
-from flask import Flask
-import jsonify
+from flask import Flask, jsonify
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb://localhost:27017/myDatabase"
+mongo = PyMongo(app)
 
 @app.route('/')
 def home():
     return {}
 
-@app.route('login')
-def login():
-    '''Login credentials'''
+@app.route('/upload')
+def upload():
+    return {}
