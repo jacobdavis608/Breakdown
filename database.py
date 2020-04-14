9c271c9f7bd276454eacaf55bccbd28dbe8f8d8a
Chris Jung <cjung@bu.edu>
2020-04-14 03:15:42 -0400

    .py made

--- database.py
+++ database.py
@@ -15,17 +15,3 @@ def home_page():
     online_users = mongo.db.users.find({"online": True})
     return render_template("index.html", online_users=online_users)
 
-@app.route("/user/<username>")
-def user_profile(username):
-    user = mongo.db.users.find_one_or_404({"_id": username})
-    return render_template("user.html",
-        user=user)
-
-@app.route("/uploads/<path:filename>")
-def get_upload(filename):
-    return mongo.send_file(filename)
-
-@app.route("/uploads/<path:filename>", methods=["POST"])
-def save_upload(filename):
-    mongo.save_file(filename, request.files["file"])
-    return redirect(url_for("get_upload", filename=filename))
