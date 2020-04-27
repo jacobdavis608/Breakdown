ea2f95c2416c1d2887710e71007a1abad6feefd6
Chris Jung <cjung@bu.edu>
2020-04-27 16:54:33 -0400

    update

--- api/app.py
+++ api/app.py
@@ -10,7 +10,9 @@ db = client.breakdown
 
 # Summary response format: 
 # {
-#    "summaries" : []
+#    "summaries" : [],
+#     "total_summaries" : integer #so end of page would say "0 to 10 of 30 Entries" 
+
 # }
 
 @app.route('/')
@@ -25,40 +27,42 @@ def get_summaries(): #url format: "https://localhost/get_summaries?user=0001&sta
     if 'user' in request.args:
         user_id = request.args['user']
     else:
-        return {"summaries": []}
+        return {"summaries": [], "total_summaries": 0}
     
     if 'start' in request.args:
         start = int(request.args['start'])
     else:
-        return {"summaries": []}
+        return {"summaries": [], "total_summaries": 0}
         
     if 'end' in request.args:
         end = int(request.args['end'])
     else:
-        return {"summaries": []}
+        return {"summaries": [], "total_summaries": 0}
 
     if (start > end):
-        return {"summaries": []}
+        return {"summaries": [], "total_summaries": 0}
 
     #get the user data
     user_data = db.users.find_one({"id" : user_id})
 
     #if user does not exist
     if user_data == None:
-        return {"summaries": []} #if frontend receives empty array, print "No entries submitted"\
+        return {"summaries": [], "total_summaries": 0} #if frontend receives empty array, print "No entries submitted"\
     else:
         num_summaries = len(user_data["summaries"])
         if (end > num_summaries):
             end = num_summaries
+        if (start > num_summaries):
+            start = 0
         if (start < 0):
             start = 0
-        
 
         summaries = user_data["summaries"][num_summaries - end: num_summaries - start]
         summaries.reverse() #put them in chronological order
 
         response = {
-            "summaries": summaries
+            "summaries": summaries,
+            "total_summaries": num_summaries
         }
         return response
 
